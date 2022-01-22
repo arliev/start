56a1fc170ec31b669f012c506c0f8f13ef4dc8d5
Xac <xac@mail.com>
2022-01-23 04:42:12 +0600

    pervyi polnochenni urok sozdanit oprosnoka

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 "use stcrict";
-alert("hello");
+
+const numberofFilm = +prompt("skolko filmov vy uzhe prosmotrely?", "");
+
+
+const personalMovieDB = {
+count: numberofFilm,
+movies:{},
+actors:{},
+genres:[],
+privat:false
+};
+
+const a = prompt('odin iz poslednig prosmotrennyh filmov?',''),
+    b = prompt('kak ochenite film?',''),
+    c = prompt ('odiz iz  poslednih prosmotrennyh filmov?', ''),
+    d = prompt ('kak ochenite film','');
+
+     personalMovieDB.movies[a] = b;
+     personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);     
