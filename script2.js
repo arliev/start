0db3cd1e0911f3b62315941bf295f13ab01c67b0
Xac <xac@mail.com>
2022-01-23 18:54:24 +0600

    Sykly v JAvaS white do i for

--- script2.js
+++ script2.js
@@ -1,41 +1,71 @@
 "use stcrict";
 
-if(4) {
-    console.log('ok!');
-} else{
-    console.log('eror');
-}
+// if(4) {
+//     console.log('ok!');
+// } else{
+//     console.log('eror');
+// }
 
 
 
-const num =50;
+// const num =50;
 
-if (num <49) {
-    console.log('eroor');
-} else if (num > 100) {
-    console.log('A lot');
-} else{
-    console.log('ok!');
-}
-
-
-
-(num === 50) ? console.log('ok!') : console.log('eroor');
+// if (num <49) {
+//     console.log('eroor');
+// } else if (num > 100) {
+//     console.log('A lot');
+// } else{
+//     console.log('ok!');
+// }
 
 
-const num = '50';
 
-switch (num) {
-    case '49':
-        console.log('wrong');
-        break;
-    case '100':
-        console.log('wrong');
-        break;
-    case '50':
-        console.log('ringht');
-        break;
-    default:
-        console.log('try again');
-        break;
-}
+// (num === 50) ? console.log('ok!') : console.log('eroor');
+
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('wrong');
+//         break;
+//     case 100:
+//         console.log('wrong');
+//         break;
+//     case 50:
+//         console.log('ringht');
+//         break;
+//     default:
+//         console.log('try again');
+//         break;
+// }
+
+
+ let num = 50;
+
+//  while (num<=55){
+//      console.log(num);
+//      num++;
+//  }
+
+
+//  do {
+// console.log(num);
+// num++;
+//  }
+//  while (num<55);
+
+
+//  for (let i = 1; i < 8; i ++){
+//      console.log(num);
+//      num++;
+//  }  
+
+
+ for (let i = 1; i < 10; i ++){
+     if (i === 6){
+        //  break;
+        continue;
+     }
+     console.log(i);
+ }  
