264320c2fb4148a322a38ce61dfa6d6c855f983a
Xac <xac@mail.com>
2022-01-23 06:58:11 +0600

    URok Usloviya

--- script2.js
+++ script2.js
@@ -1,7 +1,41 @@
 "use stcrict";
 
-if(4 == 9) {
-    console.log('ok');
+if(4) {
+    console.log('ok!');
 } else{
     console.log('eror');
+}
+
+
+
+const num =50;
+
+if (num <49) {
+    console.log('eroor');
+} else if (num > 100) {
+    console.log('A lot');
+} else{
+    console.log('ok!');
+}
+
+
+
+(num === 50) ? console.log('ok!') : console.log('eroor');
+
+
+const num = '50';
+
+switch (num) {
+    case '49':
+        console.log('wrong');
+        break;
+    case '100':
+        console.log('wrong');
+        break;
+    case '50':
+        console.log('ringht');
+        break;
+    default:
+        console.log('try again');
+        break;
 }
